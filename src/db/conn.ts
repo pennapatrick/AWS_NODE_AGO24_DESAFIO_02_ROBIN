f8f16769c3f8eaead75620baf249f824d0e19f69
Mayara Angélica <mayaraangelica411@gmail.com>
2024-10-08 16:36:22 -0300

    chore: add timezone configuration for database

--- src/db/conn.ts
+++ src/db/conn.ts
@@ -5,7 +5,8 @@ dotenv.config();
 
 const sequelize = new Sequelize(process.env.DB_NAME!, process.env.DB_USER!, process.env.DB_PASSWORD!, {
   host: process.env.DB_HOST,
-  dialect: "mysql"
+  dialect: "mysql",
+  timezone: "America/Sao_Paulo"
 });
 
 export default sequelize;
