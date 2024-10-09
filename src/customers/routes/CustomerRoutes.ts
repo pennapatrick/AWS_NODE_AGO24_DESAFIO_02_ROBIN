526ec4fecb406552980e686469eb8bc412776e3a
Mayara Angélica <mayaraangelica411@gmail.com>
2024-10-09 13:46:44 -0300

    feat: add DeleteCustomerController

--- src/customers/routes/CustomerRoutes.ts
+++ src/customers/routes/CustomerRoutes.ts
@@ -2,11 +2,13 @@ import express from 'express';
 import { createCustomer } from '../controllers/CreateCustomerController';
 import { getCustomerById } from '../controllers/GetCustomerController';
 import { getCustomers } from '../controllers/GetCustomersController';
+import { deleteCustomer } from '../controllers/DeleteCustomerController';
 
 const router = express.Router();
 
 router.post('/', createCustomer);
 router.get('/:id', getCustomerById);
 router.get('/', getCustomers);
+router.delete('/:id', deleteCustomer);
 
 export default router;
