95803443fd1c97545f5dc398fb11cbf93be87452
Patrick Penna <patrickpenna@hotmail.com>
2024-10-09 10:58:27 -0300

    feat: add updateCar route

--- src/cars/routes/carRoutes.ts
+++ src/cars/routes/carRoutes.ts
@@ -1,10 +1,11 @@
 import express from 'express';
-import { getCars, createCar, getCarById } from '../controllers/carController';
+import { getCars, createCar, getCarById, updateCar } from '../controllers/carController';
 
 const router = express.Router();
 
 router.get('/', getCars);
 router.get('/:id', getCarById);
 router.post('/', createCar);
+router.patch('/:id', updateCar);
 
 export default router;
