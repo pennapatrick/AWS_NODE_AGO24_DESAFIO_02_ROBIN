67b1a23d0756a63b3d8d32248c99f1adc62abcc0
Patrick Penna <patrickpenna@hotmail.com>
2024-10-10 12:36:52 -0300

    feat: implementing getCarByID at car.routes

--- src/routes/cars/car.routes.ts
+++ src/routes/cars/car.routes.ts
@@ -3,6 +3,7 @@ import { createCar } from "../../controllers/cars/CreateCarController";
 import { updateCar } from "../../controllers/cars/UpdateCarController";
 import { deleteCarById } from "../../controllers/cars/DeleteCarController";
 import { getCars } from "../../controllers/cars/ListCarController";
+import { getCarById } from "../../controllers/cars/ListOneCarController";
 
 const router = Router();
 
@@ -10,5 +11,6 @@ router.post('/', createCar);
 router.put('/:id', updateCar);
 router.delete('/:carId', deleteCarById);
 router.get('/', getCars);
+router.get("/:id", getCarById)
 
 export default router;
