9d2356a4204e6cb5a99c96d42f196e615f21c1c1
Patrick Penna <patrickpenna@hotmail.com>
2024-10-13 18:39:41 -0300

    feat: add update order route

--- src/routes/orders/order.routes.ts
+++ src/routes/orders/order.routes.ts
@@ -3,6 +3,7 @@ import { authorize } from '../../middleware/auth.middleware';
 import { createOrder } from '../../controllers/orders/CreateOrderController';
 import { getOrderById } from '../../controllers/orders/ListOneOrderController'
 import { deleteOrder } from '../../controllers/orders/DeleteOrderController';
+import { updateOrder } from '../../controllers/orders/UpdateOrderController';
 import { celebrate } from 'celebrate';
 import { orderCreateValidationSchema } from '../../validations/orders/OrderValidations';
 
@@ -11,5 +12,6 @@ const router = express.Router();
 router.post('/', celebrate(orderCreateValidationSchema), authorize, createOrder);
 router.get('/:id', authorize, getOrderById)
 router.delete('/:id', authorize, deleteOrder);
+router.patch('/:id', authorize, updateOrder);
 
 export default router;
