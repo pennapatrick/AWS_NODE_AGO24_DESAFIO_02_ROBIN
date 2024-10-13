f6347dba3742fd9a5573e4d653eea28ca112806b
Mayara Angélica <mayaraangelica411@gmail.com>
2024-10-13 12:45:58 -0300

    feat: add getOrderById endpoint

--- src/routes/orders/order.routes.ts
+++ src/routes/orders/order.routes.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import { authorize } from '../../middleware/auth.middleware';
 import { createOrder } from '../../controllers/orders/CreateOrderController';
+import { getOrderById } from '../../controllers/orders/ListOneOrderController'
 import { deleteOrder } from '../../controllers/orders/DeleteOrderController';
 import { celebrate } from 'celebrate';
 import { orderCreateValidationSchema } from '../../validations/orders/OrderValidations';
@@ -8,6 +9,7 @@ import { orderCreateValidationSchema } from '../../validations/orders/OrderValid
 const router = express.Router();
 
 router.post('/', celebrate(orderCreateValidationSchema), authorize, createOrder);
+router.get('/:id', authorize, getOrderById)
 router.delete('/:id', authorize, deleteOrder);
 
 export default router;
