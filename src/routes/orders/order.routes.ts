8c49c28a86aae49c15adfd587f3120f4f3b195b9
Mayara Angélica <mayaraangelica411@gmail.com>
2024-10-13 10:08:58 -0300

    feat: add deleteOrder endpoint

--- src/routes/orders/order.routes.ts
+++ src/routes/orders/order.routes.ts
@@ -1,11 +1,13 @@
 import express from 'express';
 import { authorize } from '../../middleware/auth.middleware';
 import { createOrder } from '../../controllers/orders/CreateOrderController';
+import { deleteOrder } from '../../controllers/orders/DeleteOrderController';
 import { celebrate } from 'celebrate';
 import { orderCreateValidationSchema } from '../../validations/orders/OrderValidations';
 
 const router = express.Router();
 
 router.post('/', celebrate(orderCreateValidationSchema), authorize, createOrder);
+router.delete('/:id', authorize, deleteOrder);
 
 export default router;
